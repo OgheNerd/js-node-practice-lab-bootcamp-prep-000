4d2ef66ecc76ffc146f952a2bda412e56cb0af9f
Oghene Michael <zonalmeet@gmail.com>
2018-01-27 19:45:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Joe"
-var height = 74
+var height = "74"
 
 console.log("Name: " + name)
 // console.log(name)
 
-console.log("Height: " +height+1)
-// console.log(height )
+console.log("Height:")
+console.log(height + 1)
